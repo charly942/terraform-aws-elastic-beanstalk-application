0ca21ee5eb90c2e39e05b29b9546f2da4cae2328
charly <carlos.gonzalez@uala.com.ar>
2022-09-20 10:43:57 -0300

    refactor ric all env [skip ci]

--- variables.tf
+++ variables.tf
@@ -31,8 +31,3 @@ variable "application_name" {
   type        = string
   description = "The name of the application to associate with this environment"
 }
-
-variable "destFileName" {
-  type        = string
-  description = "The name of the application version archive in the S3 bucket"
-}
