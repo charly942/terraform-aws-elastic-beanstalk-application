a7a3f534854833dd8125bdcabd320a911be80deb
charly <carlos.gonzalez@uala.com.ar>
2022-09-21 14:59:58 -0300

    refactor ric all env [skip ci]

--- main.tf
+++ main.tf
@@ -23,7 +23,8 @@ resource "aws_s3_bucket" "default" {
 
 resource "aws_s3_object" "default" {
   bucket = aws_s3_bucket.default.id
-  key    = "beanstalk/provisiones.zip"
+  key    = "/"
+  source = "provisiones.zip"
 }
 
 resource "aws_elastic_beanstalk_application_version" "default" {
